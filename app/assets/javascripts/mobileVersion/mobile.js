3144d5c94d4f67775849a1ba91f1c386c27eff0e
Chris Kim <chris@wundercode.net>
2014-03-09 20:03:48 -0700

    Refactored code for mobile version

--- app/assets/javascripts/mobileVersion/mobile.js
+++ app/assets/javascripts/mobileVersion/mobile.js
@@ -2,6 +2,6 @@
 //= require ../wunderTarot
 //= require ../lib/knockback-full-stack
 //= require ./vendor/infinitescroll
-//= require_tree ../views
-//= require_tree ../viewModels
+//= require_tree ./views
+//= require_tree ./viewModels
 //= require_tree ../collections
