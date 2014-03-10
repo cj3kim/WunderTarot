779b6587b3d074d6d236d28ad08816e139c31b7e
Chris Kim <chris@wundercode.net>
2014-03-09 19:42:45 -0700

    Improved the mobile scrolling experience

--- app/assets/javascripts/mobileVersion/mobile.js
+++ app/assets/javascripts/mobileVersion/mobile.js
@@ -1,6 +1,7 @@
 //= require jquery
 //= require ../wunderTarot
 //= require ../lib/knockback-full-stack
+//= require ./vendor/infinitescroll
 //= require_tree ../views
 //= require_tree ../viewModels
 //= require_tree ../collections
